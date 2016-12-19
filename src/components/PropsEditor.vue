af91e7d2de1a7e0baf36cf8e551041b06263e835
李斯宇 <lisiyu02@meituan.com>
2016-12-19 21:18:38 +0800

    add datepicker

--- src/components/PropsEditor.vue
+++ src/components/PropsEditor.vue
@@ -1,19 +1,20 @@
 <template>
-  <div>
-      <form class="form-horizontal" v-if="component && component.props" >
-          <div v-for="(v, k) in component.props" class="form-group">
-              <label class="col-md-4 control-label">{{v.label}}</label>
-              <div class="col-md-8"><input class="form-control" v-model="v._value"/></div>
-          </div>
-          <div class="form-group">
-              <datepicker v-model="date"></datepicker>
-          </div>
-      </form>
+  <div class="tworow">
+    <form class="form-horizontal" v-if="currentComponent && currentComponent.props">
+      <div v-for="(v, k) in currentComponent.props" class="form-group">
+        <label class="col-md-4 control-label">{{v.label}}</label>
+        <div class="col-md-8"><input class="form-control" v-model="v.default"/></div>
+      </div>
+      <div class="form-group">
+        <datepicker v-model="date"></datepicker>
+      </div>
+    </form>
   </div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
-import datepicker from 'vue-date'
+import { datepicker } from 'vue-date'
+import { Photoshop } from 'vue-color'
 export default {
   name: 'propsEditor',
   data: function () {
@@ -27,10 +28,10 @@ export default {
   },
   computed: {
     ...mapGetters({
-        componentModels: 'componentModels',
-        pageComponents: 'pageComponents',
-        currentComponent: 'currentComponent',
-        componentsMap: 'componentsMap'
+      componentModels: 'componentModels',
+      pageComponents: 'pageComponents',
+      currentComponent: 'currentComponent',
+      componentsMap: 'componentsMap'
     })
   }
 }
