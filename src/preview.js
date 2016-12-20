1ec22a18ab13ca110c915bab318e402fd5661ada
liuyu17 <liuyu17@meituan.com>
2016-12-20 15:37:37 +0800

    update package.json

---
+++ src/preview.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lincoln on 20/12/2016.
+ */
+
+import Preview from './components/Preview'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  template: '<Preview/>',
+  components: {
+    Preview
+  }
+})
